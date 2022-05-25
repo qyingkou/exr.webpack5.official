f0fe0f17fdfc1163c494183a9940c6374e3ef229
notsetyet <notsetyet@notsetyet>
2022-05-25 16:36:18 +0800

    资源：引入和使用module.js模块

--- src/index.js
+++ src/index.js
@@ -1,32 +1,18 @@
 import _ from "lodash";
 import "./style.css";
-import Icon from "./assets/image.jpg";
-import data1 from "./assets/data.xml";
-import data2 from "./assets/data.csv";
-import data3 from "./assets/data.json";
-import data4 from "./assets/data.json5";
-import data6 from "./assets/data.yaml";
-import data5 from "./assets/data.toml";
+import module from "./module.js";
 
 function component() {
   const element = document.createElement("div");
 
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  // insert css-class
-  element.classList.add("hello");
-  // insert image
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
+  // 使用module
+  const btn = document.createElement("button");
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = module;
 
-  // 打印文本资源
-  console.log(data1);
-  console.log(data2);
-  console.log(data3);
-  console.log(data4);
-  console.log(data5);
-  console.log(data6);
+  element.appendChild(btn);
 
   return element;
 }
---
+++ src/module.js
@@ -0,0 +1,3 @@
+export default function md() {
+  console.log("module");
+}
