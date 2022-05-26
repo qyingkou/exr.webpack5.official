acb8bd16574e1e0240f317903b4fd6a9dede1283
notsetyet <notsetyet@notsetyet>
2022-05-26 12:56:03 +0800

    代码分离：使用async/await简化代码，使用内置指令来生成特殊的html属性

--- Document/splitChunk.md
+++ Document/splitChunk.md
@@ -39,3 +39,31 @@ optimization: {
 拆分动态导入的代码时，webpack 提供了 2 种方法：
 1，import()语法，Ecma 标准，推荐（内部会用到 promise，需要浏览器支持）
 2，require.ensure，webpack 遗留的方法
+
+内置指令学习：
+
+- 预获取模块
+
+```js
+// 会以<link rel="prefetch" href="xxx.js"> 方式并追加到页面头部，指示着浏览器在闲置时间预取 xxx .js文件。
+import(/* webpackPrefetch: true */ "./path/to/modal.js");
+```
+
+- 预加载模块
+
+```js
+import(/* webpackPrefetch: true */ "./path/to/modal.js");
+```
+
+prefetch 指令和 preload 指令的不同之处：
+
+- preload chunk 会在父 chunk 加载时，以并行方式开始加载。
+而 prefetch chunk 会在父 chunk 加载结束后开始加载。
+
+- preload chunk 具有中等优先级，并立即下载。
+而 prefetch chunk 在浏览器闲置时下载。
+
+- preload chunk 会在父 chunk 中立即请求，用于当下时刻。
+而 prefetch chunk 会用于未来的某个时刻。
+
+- 浏览器支持程度不同。
--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-function getComponent() {
-  return (
-    import("lodash")
-      // 之所以需要 default是因为 webpack 4 在导入 CommonJS 模块时，将不再解析为 module.exports 的值，而是为 CommonJS 模块创建一个 artificial namespace 对象
-      // 参考：webpack 4: import() and CommonJs
-      .then(({ default: _ }) => {
-        const element = document.createElement("div");
-        element.innerHTML = _.join(["Hello", "webpack"], " ");
-        return element;
-      })
-      .catch((error) => "An error occurred while loading the component")
-  );
+async function getComponent() {
+  // 使用webpack的内置指令：webpackPrefetch，webpackPreload
+  const { default: _ } = await import(/* webpackPrefetch: true */ "lodash"); // !
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  return element;
 }
 getComponent().then((component) => {
   document.body.appendChild(component);
